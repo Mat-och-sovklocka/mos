af6991ee5c76b6019dc542a1e80e282c43400b71
Hernan Leiva <hleiva@hotmail.com>
2025-08-20 14:19:23 +0200

    reminder

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Reminders from './Reminders.jsx' // Importera komponenten
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <h1>Välkommen till MOS</h1>
-    
+      <Reminders /> {/* Lägg till komponenten här */}
     </>
   )
 }
