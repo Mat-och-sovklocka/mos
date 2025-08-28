d5bfb99a238460ce6bce40b7cb47957e671e6980
Hernan Leiva <hleiva@hotmail.com>
2025-08-28 16:19:28 +0200

    home component and pictures

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Välkommen till Home-sidan!</p>
+    </div>
+  );
+};
+
+export default Home;
