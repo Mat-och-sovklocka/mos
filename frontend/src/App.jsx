469ea00c2f74962aa0772b8f1357c1a5f4eb3419
Hernan Leiva <hleiva@hotmail.com>
2025-08-29 20:42:02 +0200

    positionering av bilderna

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Reminders from './Reminders.jsx' // Importera komponenten
+import Home from './Home.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Reminders /> {/* Lägg till komponenten här */}
+      <Home /> {/* Lägg till komponenten här */}
     </>
   )
 }
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./home.css";
 
 const Home = () => {
+  const [text, setText] = useState("");
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Välkommen till Home-sidan!</p>
+    <div className="ring">
+      <div className="text-container">
+        <span className="text">{text}</span>
+      </div>
+
+      <div
+        className="image allergies"
+        onMouseOver={() => {
+          setText("Allergier och specialkost");
+        }}
+      >
+      </div>
+
+      <div
+        className="image home"
+        onMouseOver={() => {
+          setText("Home");
+        }}
+      >
+        
+      </div>
+      <div
+        className="image recepies"
+        onMouseOver={() => {
+          setText("Matförslag");
+        }}
+      >
+      </div>
+      <div
+        className="image reminders_handle"
+        onMouseOver={() => {
+          setText("Lägg påminnelser");
+        }}
+      >
+      </div>
+      <div
+        className="image reminders_list"
+        onMouseOver={() => {
+          setText("Påminnelselista");
+        }}
+      >
+      </div>
+      <div
+        className="image statistics"
+        onMouseOver={() => {
+          setText("Statistik");
+        }}
+      >
+      </div>
     </div>
   );
 };
