64a93dcba2e164fa283e337b726fd113f52fde79
Eliza Zadura <eliza.zadura@gmail.com>
2025-09-08 18:15:51 +0200

    Added repository interfaces for User, Reminder, and MealRequirement

---
+++ backend/src/main/java/com/attendo/mos/repo/ReminderRepository.java
@@ -0,0 +1,11 @@
+package com.attendo.mos.repo;
+
+import com.attendo.mos.entity.Reminder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ReminderRepository extends JpaRepository<Reminder, UUID> {
+    List<Reminder> findByUserIdOrderByTimeAsc(UUID userId);
+}
